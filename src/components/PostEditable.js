ea42d70826112140e39d8c7addd80a9578b9d622
Dennis Mende <programming.dmende@hotmail.com>
2018-06-21 15:07:46 +0200

    feat: add input fields for the editing of posts

--- src/components/PostEditable.js
+++ src/components/PostEditable.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import {
   Button,
   Dropdown,
+  Form,
   Grid,
+  Input,
   Item,
+  TextArea,
 } from 'semantic-ui-react';
 
 class PostEditable extends Component {
@@ -12,37 +15,39 @@ class PostEditable extends Component {
     return (
       <React.Fragment>
         <Item.Content>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={14}>
-                <Item.Header as='a'>title</Item.Header>
-              </Grid.Column>
-              <Grid.Column width={2} textAlign='right'>
-                <Dropdown icon='ellipsis horizontal'>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>delete post</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-
-          <Item.Meta>author at timestamp</Item.Meta>
-          <Item.Description>
-            post body
-          </Item.Description>
-          <Item.Extra>
-
-            <hr></hr>
-
+          <Form>
             <Grid>
-              <Grid.Row className='post_extra_grid' verticalAlign='middle'>
-                <Grid.Column textAlign='right'>
-                  <Button primary>save changes</Button>
+              <Grid.Row>
+                <Grid.Column width={14}>
+                  <Item.Header as='a'><Input placeholder='title' /></Item.Header>
+                </Grid.Column>
+                <Grid.Column width={2} textAlign='right'>
+                  <Dropdown icon='ellipsis horizontal'>
+                    <Dropdown.Menu>
+                      <Dropdown.Item>delete post</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
-          </Item.Extra>
+
+            <Item.Meta><Input placeholder='author at timestamp' /></Item.Meta>
+            <Item.Description>
+              <TextArea placeholder='post body' />
+            </Item.Description>
+            <Item.Extra>
+
+              <hr></hr>
+
+              <Grid>
+                <Grid.Row className='post_extra_grid' verticalAlign='middle'>
+                  <Grid.Column textAlign='right'>
+                    <Button primary>save changes</Button>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Item.Extra>
+          </Form>
         </Item.Content>
       </React.Fragment>
     );
