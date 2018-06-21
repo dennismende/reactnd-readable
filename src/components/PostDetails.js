7c64c7d149ad4dcf612b1bea371fc119013bc5b0
Dennis Mende <programming.dmende@hotmail.com>
2018-06-21 15:09:05 +0200

    refactor: cleanup postdetails component

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import {
-  Comment,
-  Header,
   Item,
 } from 'semantic-ui-react';
 import Post from './Post';
 import CommentList from './CommentList';
-import noImage from '../images/no-image.png';
 
 class PostDetails extends Component {
-  state = {
-    inEditMode: false
-  }
 
   render() {
-
-    const { inEditMode } = this.state;
 
     return (
       <div>
